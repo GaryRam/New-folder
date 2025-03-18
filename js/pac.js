7b71997e2f3b42d30ff07a8c8265ba78b219f585
GaryRam <126gramirez@frhsd.com>
2025-03-18 14:02:00 -0400

    manpac

--- js/pac.js
+++ js/pac.js
@@ -22,6 +22,52 @@ class Boundary {
     }
 }
 
+class Player {
+constructor({position, velocity}) {
+    this.position = position
+    this.velocity = velocity
+    this.radius = 15
+    }
+
+draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'yellow'
+    c.fill()
+    c.closePath()
+}
+
+update() {
+    this.draw()
+}
+}
+
+
+const boundaries = []
+const player = new Player({
+    position: {
+        x:Boundary.width + Boundary.width / 2,
+        y:Boundary.height + Boundary.height / 2
+    },
+    velocity: {
+        x:0,
+        y:0
+    }
+})
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
 
 const map = [
     ['-', '-', '-', '-', '-', '-'],
@@ -30,7 +76,6 @@ const map = [
     ['-', ' ', ' ', ' ', ' ', '-'],
     ['-', '-', '-', '-', '-', '-']
 ]
-const boundaries = []
 
 map.forEach ((row, i) => {
     row.forEach((symbol, j) => {
@@ -43,11 +88,70 @@ map.forEach ((row, i) => {
                         y:Boundary.height * i
                     }
                 }))
-                break;
+                break
         }
     })
 })
 
-boundaries.forEach((boundary) => {
-    boundary.draw();
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    boundaries.forEach((boundary) => {
+        boundary.draw();
+    })
+    
+    player.update()
+    player.velocity.y = 0
+    
+    if (keys.w.pressed) {
+        player.velocity.y = -5
+    } else if (keys.a.pressed) {
+        player.velocity.x = -5
+    }
+    else if (keys.s.pressed) {
+        player.velocity.x = -5
+    }
+    else if (keys.a.pressed) {
+        player.velocity.x = -5
+    }
+}
+
+animate()
+
+
+
+addEventListener('keydown', ({key}) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = true
+        break
+        case 'a':
+            keys.a.pressed = true
+        break
+        case 's':
+            keys.s.pressed = true
+        break
+        case 'd':
+            keys.d.pressed = true
+        break
+    }
+
+})
+
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'w':
+            keys.w.pressed = false
+        break
+        case 'a':
+            keys.a.pressed = false
+        break
+        case 's':
+            keys.s.pressed = false
+        break
+        case 'd':
+            keys.d.pressed = false
+        break
+    }
+
 })
