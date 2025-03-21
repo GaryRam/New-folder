d223c5567acfba380e9eff1e33435c3ce72fba69
GaryRam <126gramirez@frhsd.com>
2025-03-21 13:46:21 -0400

    not

--- js/pac.js
+++ js/pac.js
@@ -79,6 +79,8 @@ const map = [
     ['-', ' ', ' ', ' ', ' ', ' ', '-'],
     ['-', ' ', '-', ' ', '-', ' ', '-'],
     ['-', ' ', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', '-', ' ', '-', ' ', '-'],
+    ['-', ' ', ' ', ' ', ' ', ' ', '-'],
     ['-', '-', '-', '-', '-', '-', '-']
 ]
 
@@ -102,15 +104,15 @@ function circleCollidesWithRectangle({
     circle,
     rectangle
 }) {
-    return (
-        player.position.y - player.radius + player.velocity.y <= 
-        boundary.position.y + boundary.height && 
-        player.position.x + player.radius + player.velocity.x >= 
-        boundary.position.x && 
-        player.position.y + player.radius + player.velocity.y >= 
-        boundary.position.y && 
-        player.position.x - player.radius + player.velocity.x <= 
-        boundary.position.x + boundary.width
+return (
+        circle.position.y - circle.radius + circle.velocity.y <= 
+        rectangle.position.y + rectangle.height && 
+        circle.position.x + circle.radius + circle.velocity.x >= 
+        rectangle.position.x && 
+        circle.position.y + circle.radius + circle.velocity.y >= 
+        rectangle.position.y && 
+        circle.position.x - circle.radius + circle.velocity.x <= 
+        rectangle.position.x + rectangle.width
     )
 }
 
@@ -124,26 +126,49 @@ function animate() {
     c.clearRect(0, 0, canvas.width, canvas.height)
 
         if(keys.w.pressed && lastKey === 'w') {
-            player.velocity.y = -5
+    for (let i = 0; i < boundaries.length; i++) {
+        const boundary = boundaries[i]
+    if (
+    circleCollidesWithRectangle({
+    circle: {
+        ...player,
+        velocity: {
+        x:0,
+        y:5
+    }
+   },
+    rectangle: boundary
+    })
+    )  {
+        player.velocity.y = 0
+        break
+    } else{
+        player.velocity.y = 5
+    }
+}
         } else if (keys.a.pressed && lastKey === 'a') {
             player.velocity.x = -5
         } else if (keys.s.pressed && lastKey === 's') {
-            player.velocity.y = 5
-        } else if (keys.a.pressed && lastKey === 'd') {
+
+        } else if (keys.d.pressed && lastKey === 'd') {
             player.velocity.x = 5
         }
 
 boundaries,forEach((boundary) => {
     boundary.draw()
 
-    if(
-        circleCollidesWithRectangle
-    ) {
-        player.velocity.x = 0
-        player.velocity.y = 0
-    }
+   if (
+    circleCollidesWithRectangle({
+        circle: player,
+        rectangle: boundary
+    })
+   ) {
+    player.velocity.x = 0
+    player.velocity.y = 0
+   }
 })
 player.update()
+
 }
 
 
@@ -153,19 +178,19 @@ addEventListener('keydown', ({key}) => {
     switch (key) {
         case 'w':
             keys.w.pressed = true
-            lastkey = 'w'
+            lastKey = 'w'
         break
         case 'a':
             keys.a.pressed = true
-            lastkey = 'a'
+            lastKey = 'a'
         break
         case 's':
             keys.s.pressed = true
-            lastkey = 's'
+            lastKey = 's'
         break
         case 'd':
             keys.d.pressed = true
-            lastkey = 'd'
+            lastKey = 'd'
         break
     }
 
